9fea79c7eab00e6ff1e48b82c31944320be897f9
Linxx333 <nachoromerogsd@gmail.com>
2023-06-08 23:47:59 +0200

    mas querys

--- src/SQL/Querys Mr Linx.sql
+++ src/SQL/Querys Mr Linx.sql
@@ -3,6 +3,18 @@ VALUES (1, 'Superliga');
 
 --     PLAYER VS PLAYER   --
 
+-- KDA Vs Player --
+
+select p.nick as Player, pl.nick as PlayersVs, sum(gp.Kills)/2 as 'Total Kills',sum(gp.Deaths)/2 as 'Total Deaths',sum(gp.Assists)/2 as 'Total Assists',  (((sum(gp.Kills)/2) + (sum(gp.Assists)/2))/(sum(gp.Deaths)/2)) as KDA 
+from games_teams as gt
+inner join players as p
+on gt.player_id4 = p.player_id
+inner join players as pl
+on gt.player_id9 = pl.player_id
+inner join games_players as gp
+on p.player_id = gp.players_id
+where p.nick = 'Flakked' and pl.nick = 'Attila'and gt.teamVs_id = gp.teamVs_id and gt.split_id = gp.split_id;
+
 -- Win Ratio --
 
 select p.nick as Player, pl.nick as PlayerVs, (AVG(gt.Winned)*100) as Winrate 
@@ -67,3 +79,61 @@ from games_players as gp
 inner join players as p
 on gp.players_id = p.player_id
 where p.nick = 'Attila';
+
+-- Veces Ganador del split --
+
+select p.nick as Jugador,count(p.nick) as Títulos
+from players as p
+inner join `hist-players` as hp
+on p.player_id =  hp.players_id 
+inner join splits as s
+on hp.split_id = s.split_id
+inner join winners as w
+on s.split_id = w.split_id 
+where p.nick = 'Miniduke' and hp.split_id = w.split_id and hp.team_id = w.winnerteam_id;
+
+-- JUGADOR VS EQUIPO --
+
+-- KDA Vs Equipo --
+
+select p.nick as Jugador, t.name as EquipoVs, sum(gp.Kills) as 'Total Kills',sum(gp.Deaths) as 'Total Deaths',sum(gp.Assists) as 'Total Assists',  ((sum(gp.Kills) + sum(gp.Assists))/sum(gp.Deaths)) as KDA 
+from games_players as gp
+inner join players as p
+on gp.players_id = p.player_id
+inner join teams as t
+on gp.teamVs_id = t.team_id
+where p.nick = 'Attila' and t.name = 'Movistar Riders'; 
+
+-- Winrate Vs Equipo -- 
+
+select p.nick as Jugador, avg(gt.winned)*100 as 'Win Ratio'
+from games_players as gp
+inner join players as p
+on gp.players_id = p.player_id
+inner join teams as t
+on gp.teamVs_id = t.team_id
+inner join games_teams as gt
+on t.team_id = gt.teamVs_id
+where p.nick = 'Attila' and t.name = 'Movistar Riders' and gp.team_id = gt.team_id and gp.players_id = gt.player_id4; 
+
+-- EQUIPO VS EQUIPO --
+
+-- Nº de Encuentros --
+
+select t.name as Equipo, tt.name as EquipoVs, count(tt.name) as 'Nº de Encuentros'
+from games_teams as gt
+inner join teams as t
+on gt.team_id = t.team_id
+inner join teams as tt
+on gt.teamVs_id = tt.team_id
+where t.name = 'Giants' and tt.name = 'Guasones';
+
+-- Winrate Equipo Vs Equipo --
+
+select t.name as Equipo, tt.name as EquipoVs, avg(gt.Winned)*100 as 'Win Ratio'
+from games_teams as gt
+inner join teams as t
+on gt.team_id = t.team_id
+inner join teams as tt
+on gt.teamVs_id = tt.team_id
+where t.name = 'Giants' and tt.name = 'BISONS ECLUB';
